deb7a99322478affd70bcb5edc32c4fcfc9423fe
sleng17 <sammert17@yandex.ru>
2019-07-30 12:32:58 +0300

    input - shorter

--- ft_printf.c
+++ ft_printf.c
@@ -12,85 +12,119 @@
 
 #include "ft_printf.h"
 
-int					ft_type_definition(const char **str,
-					t_spec *list, va_list per)
-{
-	(*str)++;
-	return (1);
-}
 
-void				ft_check_width_and_precision(const char **str,
-										t_spec *list, va_list per)
+
+void get_format1(const char **str, t_spec *format, va_list argptr)
 {
+	format->presence_dot = **str == '.';
+	*str += **str == '.';
+	if (**str == '*' || ft_isdigit(**str))
+		format->presence_precision = 1;
+	format->precision = ft_atoi(*str);
 	if (**str == '*')
+		format->precision = va_arg(argptr, int);
+	while (**str == '*' || ft_isdigit(**str))
+		++*str;
+	if (**str == 'h' && *(*str + 1) == 'h')
+		format->modificate = CHAR;
+	else if (**str == 'h')
+		format->modificate = SHORT;
+	else if (**str == 'l' && *(*str + 1) == 'l')
+		format->modificate = LONG_LONG;
+	else if (**str == 'l')
+		format->modificate = LONG;
+	else if (**str == 'L')
+		format->modificate = LONG_DOUBLE;
+	while (**str == 'h' || **str == 'l' || **str == 'L')
+		++*str;
+	format->type = **str;
+	++*str;
+}
+
+void get_format(const char **str, t_spec *format, va_list argptr)
+{
+	ft_memset(format, 0, sizeof(t_spec));
+	while (**str == '-' || **str == '+' || **str == ' ' ||
+			**str == '#' || (**str == '0' && !format->flag_zero))
 	{
-		list->width = va_arg(per, int);
-		list->presence_width = 1;
-		(*str)++;
+		if (**str == '-')
+			format->flag_minus = 1;
+		else if (**str == '+')
+			format->flag_plus = 1;
+		else if (**str == ' ')
+			format->flag_space = 1;
+		else if (**str == '#')
+			format->flag_hash = 1;
+		else if (**str == '0')
+			format->flag_zero = 1;
+		++*str;
 	}
-	while (ft_isdigit(**str))
-	{
-		list->presence_width = 1;
-		(*str)++;
-	}
-	if (**str == '.')
-	{
-		list->precision = ft_atoi(++(*str));
-		list->presence_dot = 1;
-	}
+	format->width = ft_atoi(*str);
 	if (**str == '*')
-	{
-		list->precision = va_arg(per, int);
-		list->presence_precision = 1;
-		(*str)++;
-	}
+		format->width = va_arg(argptr, int);
+	if (**str == '*' || ft_isdigit(**str))
+		format->presence_width = 1;
+	while (**str == '*' || ft_isdigit(**str))
+		++*str;
+	get_format1(str, format, argptr);
 }
 
-int					ft_second_step(const char **str, va_list per, t_spec *list)
+int					print_arg(const char **str, va_list argptr)
 {
-		if (!(ft_check_format(**str)))
-			ft_check_flags(str, list, per);
-		ft_check_width_and_precision(str, list, per);
-		while (ft_isdigit(**str))
-		{
-			(*str)++;
-			list->presence_precision = 1;
-		}
-		ft_check_modificate(str, list);
-		return (ft_type_definition(str, list, per));
+	int		return_value;
+	t_spec	format;
+
+//	va_arg(argptr, int);
+
+	get_format(str, &format, argptr);
+	return_value = 0;
+	printf("flag_minus: %u\n", format.flag_minus);
+	printf("flag_plus: %u\n", format.flag_plus);
+	printf("flag_space: %u\n", format.flag_space);
+	printf("flag_zero: %u\n", format.flag_zero);
+	printf("flag_hash: %u\n", format.flag_hash);
+	printf("precision: %u\n", format.precision);
+	printf("precision_presence: %u\n", format.presence_precision);
+	printf("dot_presence: %u\n", format.presence_dot);
+	printf("width_presence: %u\n", format.presence_width);
+	printf("width: %u\n", format.width);
+	printf("type: %c\n", format.type);
+	printf("modifier: %u\n", format.modificate);
+
+	return (return_value);
 }
 
-int 		ft_first_step(const char *str, va_list per)
+int 		print(const char *str, va_list argptr)
 {
-	const char		*output;
-	t_spec			*list;
+	const char		*prev_pos;
 	int				return_value;
 
-	output = str;
-	list = ft_list_new();
+	prev_pos = str;
 	return_value = 0;
-	while (*str)
+	while (*prev_pos)
 	{
 		while (*str && *str != '%')
-			str++;
-		write (1, output, str - output);
-		return_value += (str - output);
-		str++;
-		if(*str)
-			return_value += ft_second_step(&str, per, list);
-		output = str;
-		ft_list_clear(list);
+			++str;
+		write(1, "***\n", 4);
+		write (1, prev_pos, str - prev_pos);
+		write(1, "\n***\n", 5);
+		return_value += str - prev_pos;
+		if (*str) {
+			++str;
+			return_value += print_arg(&str, argptr);
+		}
+		prev_pos = str;
 	}
 	return (return_value);
 }
 
 int 		ft_printf(const char *str, ...)
 {
-	va_list		per;
+	va_list		argptr;
 	int			return_value;
 
-	va_start(per, str);
-	return_value = ft_first_step(str, per);
-	va_end(per);
+	va_start(argptr, str);
+	return_value = print(str, argptr);
+	va_end(argptr);
 	return (return_value);
 }
