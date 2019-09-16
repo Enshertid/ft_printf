efb53bea57bc5a3aa5018b688e6c93669a434a8a
Dondarrion Bendu <dbendu@at-i2.21-school.ru>
2019-09-16 18:50:00 +0300

    libft under norm

--- main.c
+++ main.c
@@ -2,6 +2,32 @@
 #include "math.h"
 #include "ft_printf.h"
 
+char	*get_binary(void *ptr, t_spec *format)
+{
+	char	*str;
+	size_t size;
+
+	str = (char*)malloc(format->)
+}
+	void	print_bits(void *a, size_t size)
+{
+	char	buf[size * 8 + size];
+	size_t	pos;
+	int		byte;
+	int		offset;
+
+	pos = 0;
+	byte = size;
+	while (--byte >= 0)
+	{
+		offset = 8;
+		while (--offset >= 0)
+			buf[pos++] = '0' + ((*((char*)a + byte) >> offset) & 1);
+		buf[pos++] = ' ';
+	}
+	write(1, buf, size * 8 + size);
+}
+
 int			main(void)
 {
 	long double d = 44.5 + 1e-52;
